c71a760b11082ec68776a672d109fb3d11857f82
Ludwig-10 <70726454+Ludwig-10@users.noreply.github.com>
2021-12-09 22:47:12 +0000

    Deploying to gh-pages from @ Ludwig-10/galeria-meubelmay@d36d031dda110edc6e4b9c734c15f49c7b464403 🚀

--- servise.fc19ae6f.js
+++ servise.fc19ae6f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/BGmin.png":[["BGmin.16ca0a20.png","YkNa"],"YkNa"]}],"FbOa":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["FbOa"], null)
-//# sourceMappingURL=/galeria-meubelmay/servise.1800d525.js.map
+//# sourceMappingURL=/galeria-meubelmay/servise.fc19ae6f.js.map
